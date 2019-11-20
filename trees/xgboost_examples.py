401b03659c10e00cff82a7db7d0b889cb0c592fc
David Sheffield <david@davidsheffield.net>
2019-11-19 20:08:21 -0500

    Add multi:softprob to xgboost example

--- trees/xgboost_examples.py
+++ trees/xgboost_examples.py
@@ -20,16 +20,29 @@ def example_1(objective):
     X_train = np.array([[0],
                         [1]])
     y_train = np.array([0, 1])
+    if objective == 'multi:softprob':
+        X_train = np.array([[0],
+                            [1],
+                            [2]])
+        y_train = np.array([0, 1, 2])
     data_train = xgb.DMatrix(X_train, y_train)
 
     X_test = np.array([[0],
                        [1],
                        [np.nan]])
     y_test = np.array([0, 1, 0])
+    if objective == 'multi:softprob':
+        X_test = np.array([[0],
+                           [1],
+                           [2],
+                           [np.nan]])
+        y_test = np.array([0, 1, 2, 0])
     data_test = xgb.DMatrix(X_test, y_test)
 
     parameters = PARAMETERS.copy()
     parameters['objective'] = objective
+    if objective == 'multi:softprob':
+        parameters['num_class'] = 3
 
     bst = xgb.train(parameters,
                     data_train,
@@ -44,3 +57,4 @@ def example_1(objective):
 
 if __name__ == '__main__':
     example_1('binary:logistic')
+    example_1('multi:softprob')
