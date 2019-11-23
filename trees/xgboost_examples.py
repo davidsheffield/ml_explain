133a3c851fe599874724a8843f5d79d90c1a3957
David Sheffield <david@davidsheffield.net>
2019-11-23 18:39:13 -0500

    Add mean squared error objective

--- trees/xgboost_examples.py
+++ trees/xgboost_examples.py
@@ -7,7 +7,7 @@ import xgboost as xgb
 
 
 PARAMETERS = {'max_depth': 2,
-              'eta': 0.1,
+              'eta': 0.5,
               'lambda': 1,
               'gamma': 0,
               'min_child_weight': 0}
@@ -17,32 +17,44 @@ def example_1(objective):
     Simple data
     """
 
-    X_train = np.array([[0],
-                        [1]])
-    y_train = np.array([0, 1])
-    if objective == 'multi:softprob':
+    parameters = PARAMETERS.copy()
+    parameters['objective'] = objective
+
+    if objective == 'reg:squarederror':
         X_train = np.array([[0],
                             [1],
                             [2]])
         y_train = np.array([0, 1, 2])
-    data_train = xgb.DMatrix(X_train, y_train)
-
-    X_test = np.array([[0],
-                       [1],
-                       [np.nan]])
-    y_test = np.array([0, 1, 0])
-    if objective == 'multi:softprob':
+        X_test = np.array([[-1],
+                           [0],
+                           [1],
+                           [2],
+                           [3]])
+        y_test = np.array([-1, 0, 1, 2, 3])
+    elif objective == 'binary:logistic':
+        X_train = np.array([[0],
+                            [1]])
+        y_train = np.array([0, 1])
+        X_test = np.array([[0],
+                           [1],
+                           [np.nan]])
+        y_test = np.array([0, 1, 0])
+    elif objective == 'multi:softprob':
+        X_train = np.array([[0],
+                            [1],
+                            [2]])
+        y_train = np.array([0, 1, 2])
         X_test = np.array([[0],
                            [1],
                            [2],
                            [np.nan]])
         y_test = np.array([0, 1, 2, 0])
-    data_test = xgb.DMatrix(X_test, y_test)
-
-    parameters = PARAMETERS.copy()
-    parameters['objective'] = objective
-    if objective == 'multi:softprob':
         parameters['num_class'] = 3
+    else:
+        raise ValueError('Not a recognized objective')
+
+    data_train = xgb.DMatrix(X_train, y_train)
+    data_test = xgb.DMatrix(X_test, y_test)
 
     bst = xgb.train(parameters,
                     data_train,
@@ -56,5 +68,6 @@ def example_1(objective):
 
 
 if __name__ == '__main__':
+    example_1('reg:squarederror')
     example_1('binary:logistic')
     example_1('multi:softprob')
